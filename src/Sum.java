75aef12bd3b012fb7687893495856d7c02e7c4eb
MEDISETTILOKESH <117367248+MEDISETTILOKESH@users.noreply.github.com>
2022-11-24 19:46:59 +0530

    Added code

---
+++ src/Sum.java
@@ -0,0 +1,11 @@
+Class Sum{
+public static void main(string arg[])
+{
+int num1,num2,res;
+Scanner s = new Scanner(System.in);
+System.out.println("Enter num1 value");
+num1=s.nextInt();
+res=num1+num2;
+System.out.println("Sum is : : "+res");
+}
+}
